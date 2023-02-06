9b80570ecfa517cae702ab0dffffd76907d9232c
Ljubomir Francuski <ljubomir.francuski@gmail.com>
2023-02-06 01:29:13 +0100

    Committing

---
+++ Testing.R
@@ -0,0 +1,4 @@
+print("Created from RStudio")
+print("In project of type version control which is given repo url ... ")
+print("ssh key is created in RStudio and given to github account.")
+print("So this file will live on repo.")
